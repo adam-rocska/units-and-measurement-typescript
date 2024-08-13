a2cf062f6ec82cc1453254d3ba7c80949701f06a
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-08-13 17:38:49 +0200

    🏗️ Adding this failing test to pick up where I left off. Got a train to catch.

---
+++ test/user/simple-shorthand-example.test.ts
@@ -0,0 +1,21 @@
+import {inches, centimeters} from '@adam-rocska/units-and-measurement/shorthand';
+
+test("The simplest example.", () => {
+  const paperWidth = inches(5.5);
+  const paperHeight = inches(11);
+
+  /// MARK: With untouched floating point results
+  expect(paperWidth.convertedTo(centimeters))
+    .toEqual(centimeters(8.5 * (((127 / 5) / 1000)) * 100));
+
+  expect(paperHeight.convertedTo(centimeters))
+    .toEqual(centimeters(11 * (((127 / 5) / 1000)) * 100));
+
+  /// MARK: With rounded floating point results
+  expect(paperWidth.convertedTo(centimeters).toFixed(3))
+    .toEqual(centimeters(21.59));
+
+  expect(paperHeight.convertedTo(centimeters).toFixed(3))
+    .toEqual(centimeters(27.94));
+
+});
