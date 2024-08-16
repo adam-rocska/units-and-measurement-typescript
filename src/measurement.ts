90019f9ae8b3a9fa1ef5ddcc65565e81c5705cf2
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-08-16 15:38:05 +0200

    🏗️ Measurement representations.

--- src/measurement.ts
+++ src/measurement.ts
@@ -31,8 +31,28 @@ export type MeasurementString<
   UnitType extends Unit,
   Value extends number = number
 > = `${Value}${UnitType}`;
+export function MeasurementString<
+  UnitType extends Unit,
+  Value extends number = number
+>(from: Measurement<UnitType, Value>): MeasurementString<UnitType, Value> {
+  if (from instanceof Array) return `${from[0]}${from[1]}`;
+  if (typeof from === 'string') return from;
+  return `${from.value}${from.unit}`;
+}
 
 export type MeasurementObject<
   UnitType extends Unit,
   Value extends number = number
 > = {value: Value; unit: UnitType;};
+export function MeasurementObject<
+  UnitType extends Unit,
+  Value extends number = number
+>(from: Measurement<UnitType, Value>): MeasurementObject<UnitType, Value> {
+  if (from instanceof Array) return {value: from[0], unit: from[1]};
+  if (typeof from === 'string') {
+    const value = parseFloat(from) as Value;
+    const unit = from.slice(value.toString().length) as UnitType;
+    return {value, unit};
+  }
+  return {...from};
+}
--- test/unit/measurement.test.ts
+++ test/unit/measurement.test.ts
@@ -1,4 +1,4 @@
-import {Measurement, MeasurementTuple} from "!src/measurement";
+import {Measurement, MeasurementObject, MeasurementString, MeasurementTuple} from "!src/measurement";
 
 describe("MeasurementTuple", () => {
   describe("given a tuple", () => {
@@ -26,6 +26,53 @@ describe("MeasurementTuple", () => {
 
     it("should return a tuple equaling the measurement represented by the object.", () => {
       expect(MeasurementTuple(object)).toEqual([1, "m"]);
+    });
+  });
+});
+
+describe("MeasurementString", () => {
+  describe("given a tuple", () => {
+    const tuple: Measurement<"m", 1> = [1, "m"];
+    it("should return a string equaling the measurement represented by the tuple.", () => {
+      expect(MeasurementString(tuple)).toEqual("1m");
+    });
+  });
+
+  describe("given an object", () => {
+    const object: Measurement<"m", 1> = {value: 1, unit: "m"};
+
+    it("should return a string equaling the measurement represented by the object.", () => {
+      expect(MeasurementString(object)).toEqual("1m");
+    });
+  });
+});
+
+describe("MeasurementObject", () => {
+  describe("given a tuple", () => {
+    const tuple: Measurement<"m", 1> = [1, "m"];
+
+    it("should return an object equaling the measurement represented by the tuple.", () => {
+      expect(MeasurementObject(tuple)).toEqual({value: 1, unit: "m"});
+    });
+  });
+
+  describe("given a string", () => {
+    const string: Measurement<"m", 1> = "1m";
+
+    it("should return an object equaling the measurement represented by the string.", () => {
+      expect(MeasurementObject(string)).toEqual({value: 1, unit: "m"});
+    });
+  });
+
+  describe("given an object", () => {
+    const object: Measurement<"m", 1> = {value: 1, unit: "m"};
+
+    it("should return an object equaling the given object.", () => {
+      expect(MeasurementObject(object)).toEqual(object);
+    });
+
+    it("shouldn't return the same instance.", () => {
+      expect(MeasurementObject(object)).not.toBe(object);
     });
   });
 });
