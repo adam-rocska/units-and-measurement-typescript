1e46c5cab12c8a9003f20d3a0014815cfdc2f597
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-08-21 20:42:09 +0200

    🏗️ I have no clue what else to test for here.

---
+++ test/unit/conversion.test.ts
@@ -0,0 +1,15 @@
+import {linearConversion} from "!src/conversion";
+
+describe("linearConversion", () => {
+  it("should create a linear conversion tuple", () => {
+    const conversion = linearConversion(2, 3);
+    expect(conversion[0](5)).toBe(13);
+    expect(conversion[1](13)).toBe(5);
+  });
+
+  it("should create a linear conversion tuple with default constant", () => {
+    const conversion = linearConversion(2);
+    expect(conversion[0](5)).toBe(10);
+    expect(conversion[1](10)).toBe(5);
+  });
+});
