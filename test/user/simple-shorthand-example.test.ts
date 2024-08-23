495f0b0598b4388161ff9ca97a06d17f1bce1d2b
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-08-23 16:38:58 +0200

    🧹 Removed obsolete comments.

--- test/user/simple-shorthand-example.test.ts
+++ test/user/simple-shorthand-example.test.ts
@@ -1,12 +1,10 @@
 import {toFixed} from '@adam-rocska/units-and-measurement/measurement';
 import {inches, centimeters} from '@adam-rocska/units-and-measurement/length';
 
-/// NOTE: This test is great because it dances around with the fucking floating point arithmetic issues too.
 test("The simplest example.", () => {
   const paperWidth = inches(5.5);
   const paperHeight = inches(11);
 
-  /// MARK: With untouched floating point results
   expect(toFixed(centimeters(paperWidth), 2))
     .toEqual(centimeters(13.97));
 
