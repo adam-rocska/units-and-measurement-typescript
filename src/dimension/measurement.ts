ed414ebde73a3423e318de39bd8cafbf7e0921db
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-08-23 14:39:52 +0200

    🧹 Cleans, builds, checks, and tests well.

--- src/dimension/measurement.ts
+++ src/dimension/measurement.ts
@@ -1,10 +1,10 @@
 import {Conversions} from "../conversion";
-import {type Measurement} from "./measurement-type";
+import {type Measurement as MeasurementType} from "./measurement-type";
 import {Dimension} from "./dimension";
 import {UnitMeasurement} from "./measurement-unit";
 import {DimensionMeasurement} from "./measurement-dimension";
 
-export function Measurement<Unit extends string>(unit: Unit): Measurement<Unit>;
+export function Measurement<Unit extends string>(unit: Unit): MeasurementType<Unit>;
 export function Measurement<Unit extends string>(conversions: Conversions<Unit>): Dimension<Unit>;
 export function Measurement(description: any): any {
   return typeof description === "string"
