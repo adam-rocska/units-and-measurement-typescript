c866b11685b1d9c0a89f996eab69fbb70c3a959e
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-08-24 21:17:27 +0200

    🏗️ Added number conformance.

--- src/measurement/object.ts
+++ src/measurement/object.ts
@@ -1,26 +1,57 @@
-import * as string from "./string";
-import * as tuple from "./tuple";
+import * as s from "./string";
+import * as t from "./tuple";
 
-export class Object<Unit extends string, Value extends number> {
+export class Object<Unit extends string, Value extends number> implements Number {
   public readonly value: Value;
   public readonly unit: Unit;
 
-  constructor(measurement: string.String<Unit, Value>);
-  constructor(measurement: tuple.Tuple<Unit, Value>);
+  /// MARK: Constructor
+
+  constructor(measurement: s.String<Unit, Value>);
+  constructor(measurement: t.Tuple<Unit, Value>);
   constructor(value: Value, unit: Unit);
   constructor(...args: any[]) {
     const [value, unit] = args;
     if (unit === undefined) {
-      if (string.isString<Unit, Value>(value)) {
-        this.unit = string.unit(value);
-        this.value = string.value(value);
+      if (s.isString<Unit, Value>(value)) {
+        this.unit = s.unit(value);
+        this.value = s.value(value);
         return;
       }
-      this.unit = tuple.unit(value);
-      this.value = tuple.value(value);
+      this.unit = t.unit(value);
+      this.value = t.value(value);
       return;
     }
     this.unit = unit;
     this.value = value;
   }
+
+  /// MARK: Number Conformance
+
+  toString(radix?: number): string {
+    return this.value.toString(radix);
+  }
+
+  toFixed(fractionDigits?: number): string {
+    return this.value.toFixed(fractionDigits);
+  }
+
+  toExponential(fractionDigits?: number): string {
+    return this.value.toExponential(fractionDigits);
+  }
+
+  toPrecision(precision?: number): string {
+    return this.value.toPrecision(precision);
+  }
+  valueOf(): number {
+    return this.value;
+  }
+
+  toLocaleString(locales?: string | string[], options?: Intl.NumberFormatOptions): string;
+  toLocaleString(locales?: Intl.LocalesArgument, options?: Intl.NumberFormatOptions): string;
+  toLocaleString(locales?: unknown, options?: unknown): string;
+  toLocaleString(...args: any[]): string {
+    return this.value.toLocaleString(...args);
+  }
+
 }
--- test/unit/measurement/object.test.ts
+++ test/unit/measurement/object.test.ts
@@ -1,4 +1,5 @@
 import {Object} from "!src/measurement/object";
+import * as s from "!src/measurement/string";
 
 describe('Object', () => {
   describe("constructor", () => {
@@ -19,5 +20,54 @@ describe('Object', () => {
       expect(measurement.value).toEqual(5.5);
       expect(measurement.unit).toEqual("in");
     });
+  });
+
+  /// MARK: Number Conformance Tests
+
+  describe("Number Conformance", () => {
+    describe("toString", () => {
+      it("should return the VALUE of the measurement in the expected radix.", () => {
+        const measurement = new Object("5.5in");
+        for (let i = 2; i <= 36; i++) {
+          expect(measurement.toString(i)).toEqual((5.5).toString(i));
+        }
+      });
+    });
+
+    describe("toFixed", () => {
+      it("should return the VALUE of the measurement with the expected number of fraction digits.", () => {
+        const measurement = new Object("5.5in");
+        expect(measurement.toFixed(2)).toEqual((5.5).toFixed(2));
+      });
+    });
+
+    describe("toExponential", () => {
+      it("should return the VALUE of the measurement in exponential notation with the expected number of fraction digits.", () => {
+        const measurement = new Object("5.5in");
+        expect(measurement.toExponential(2)).toEqual((5.5).toExponential(2));
+      });
+    });
+
+    describe("toPrecision", () => {
+      it("should return the VALUE of the measurement with the expected precision.", () => {
+        const measurement = new Object("5.5in");
+        expect(measurement.toPrecision(2)).toEqual((5.5).toPrecision(2));
+      });
+    });
+
+    describe("valueOf", () => {
+      it("should return the VALUE of the measurement.", () => {
+        const measurement = new Object("5.5in");
+        expect(measurement.valueOf()).toEqual(5.5);
+      });
+    });
+
+    describe("toLocaleString", () => {
+      it("should return the VALUE of the measurement in the expected locale.", () => {
+        const measurement = new Object("5.5in");
+        expect(measurement.toLocaleString()).toEqual((5.5).toLocaleString());
+      });
+    });
+
   });
 });
