0d3ab88ee4cef6c8de1605080084375986be9d3b
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-08-24 22:44:02 +0200

    🏗️ DOPE! I could make Tuple conformance happen.

--- src/measurement/object.ts
+++ src/measurement/object.ts
@@ -1,9 +1,14 @@
 import * as s from "./string";
 import * as t from "./tuple";
 
-export class Object<Unit extends string, Value extends number> implements Number {
-  public readonly value: Value;
-  public readonly unit: Unit;
+export class Object<Unit extends string, Value extends number> implements Number, t.Tuple<Unit, Value> {
+  private measurement: t.Tuple<Unit, Value>;
+
+  public get value(): Value {return this.measurement[0];}
+  public set value(value: Value) {this.measurement = [value, this.unit];}
+
+  public get unit(): Unit {return this.measurement[1];}
+  public set unit(unit: Unit) {this.measurement = [this.value, unit];}
 
   /// MARK: Constructor
 
@@ -14,16 +19,13 @@ export class Object<Unit extends string, Value extends number> implements Number
     const [value, unit] = args;
     if (unit === undefined) {
       if (s.isString<Unit, Value>(value)) {
-        this.unit = s.unit(value);
-        this.value = s.value(value);
+        this.measurement = [s.value(value), s.unit(value)];
         return;
       }
-      this.unit = t.unit(value);
-      this.value = t.value(value);
+      this.measurement = value;
       return;
     }
-    this.unit = unit;
-    this.value = value;
+    this.measurement = [value, unit];
   }
 
   /// MARK: Number Conformance
@@ -50,8 +52,45 @@ export class Object<Unit extends string, Value extends number> implements Number
   toLocaleString(locales?: string | string[], options?: Intl.NumberFormatOptions): string;
   toLocaleString(locales?: Intl.LocalesArgument, options?: Intl.NumberFormatOptions): string;
   toLocaleString(locales?: unknown, options?: unknown): string;
-  toLocaleString(...args: any[]): string {
+  toLocaleString(...args: Parameters<Number["toLocaleString"]>): string {
     return this.value.toLocaleString(...args);
   }
+
+  /// MARK: Tuple Conformance
+
+  get 0(): Value {return this.measurement[0];}
+  get 1(): Unit {return this.measurement[1];}
+  get length(): 2 {return this.measurement.length;}
+  [n: number]: Unit | Value;
+
+  get concat() {return this.measurement.concat;}
+  get join() {return this.measurement.join;}
+  get slice() {return this.measurement.slice;}
+  get indexOf() {return this.measurement.indexOf;}
+  get lastIndexOf() {return this.measurement.lastIndexOf;}
+  get every() {return this.measurement.every;}
+  get some() {return this.measurement.some;}
+  get forEach() {return this.measurement.forEach;}
+  get map() {return this.measurement.map;}
+  get filter() {return this.measurement.filter;}
+  get reduce() {return this.measurement.reduce;}
+  get reduceRight() {return this.measurement.reduceRight;}
+  get find() {return this.measurement.find;}
+  get findIndex() {return this.measurement.findIndex;}
+  get entries() {return this.measurement.entries;}
+  get keys() {return this.measurement.keys;}
+  get values() {return this.measurement.values;}
+  get includes() {return this.measurement.includes;}
+  get flatMap() {return this.measurement.flatMap;}
+  get flat() {return this.measurement.flat;}
+  get at() {return this.measurement.at;}
+  get findLast() {return this.measurement.findLast;}
+  get findLastIndex() {return this.measurement.findLastIndex;}
+  get toReversed() {return this.measurement.toReversed;}
+  get toSorted() {return this.measurement.toSorted;}
+  get toSpliced() {return this.measurement.toSpliced;}
+  get with() {return this.measurement.with;}
+  get [Symbol.iterator]() {return this.measurement[Symbol.iterator];}
+  get [Symbol.unscopables]() {return this.measurement[Symbol.unscopables];}
 
 }
--- test/unit/measurement/object.test.ts
+++ test/unit/measurement/object.test.ts
@@ -1,7 +1,9 @@
 import {Object} from "!src/measurement/object";
 import * as s from "!src/measurement/string";
+import * as t from "!src/measurement/tuple";
 
-describe('Object', () => {
+describe("Object", () => {
+  /// MARK: Constructor tests
   describe("constructor", () => {
     it("should accept a string", () => {
       const measurement = new Object("5.5in");
@@ -68,6 +70,210 @@ describe('Object', () => {
         expect(measurement.toLocaleString()).toEqual((5.5).toLocaleString());
       });
     });
+  });
 
+  /// MARK: Tuple Conformance Tests
+  describe("Tuple Conformance", () => {
+    const unit = new Object(5.5, "in");
+    const tuple = Reflect.get(unit, "measurement");
+
+    describe("0", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit[0]).toBe(tuple[0]);
+      });
+    });
+    describe("1", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit[1]).toBe(tuple[1]);
+      });
+    });
+    describe("length", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.length).toBe(tuple.length);
+      });
+    });
+    describe("concat ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.concat).toBe(tuple.concat);
+      });
+    });
+    describe("join ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.join).toBe(tuple.join);
+      });
+    });
+    describe("slice ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.slice).toBe(tuple.slice);
+      });
+    });
+    describe("indexOf ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.indexOf).toBe(tuple.indexOf);
+      });
+    });
+    describe("lastIndexOf ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.lastIndexOf).toBe(tuple.lastIndexOf);
+      });
+    });
+    describe("every ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.every).toBe(tuple.every);
+      });
+    });
+    describe("some ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.some).toBe(tuple.some);
+      });
+    });
+    describe("forEach ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.forEach).toBe(tuple.forEach);
+      });
+    });
+    describe("map ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.map).toBe(tuple.map);
+      });
+    });
+    describe("filter ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.filter).toBe(tuple.filter);
+      });
+    });
+    describe("reduce ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.reduce).toBe(tuple.reduce);
+      });
+    });
+    describe("reduceRight ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.reduceRight).toBe(tuple.reduceRight);
+      });
+    });
+    describe("find ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.find).toBe(tuple.find);
+      });
+    });
+    describe("findIndex ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.findIndex).toBe(tuple.findIndex);
+      });
+    });
+    describe("entries ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.entries).toBe(tuple.entries);
+      });
+    });
+    describe("keys ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.keys).toBe(tuple.keys);
+      });
+    });
+    describe("values ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.values).toBe(tuple.values);
+      });
+    });
+    describe("includes ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.includes).toBe(tuple.includes);
+      });
+    });
+    describe("flatMap ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.flatMap).toBe(tuple.flatMap);
+      });
+    });
+    describe("flat ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.flat).toBe(tuple.flat);
+      });
+    });
+    describe("at ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.at).toBe(tuple.at);
+      });
+    });
+    describe("findLast ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.findLast).toBe(tuple.findLast);
+      });
+    });
+    describe("findLastIndex ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.findLastIndex).toBe(tuple.findLastIndex);
+      });
+    });
+    describe("toReversed ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.toReversed).toBe(tuple.toReversed);
+      });
+    });
+    describe("toSorted ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.toSorted).toBe(tuple.toSorted);
+      });
+    });
+    describe("toSpliced ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.toSpliced).toBe(tuple.toSpliced);
+      });
+    });
+    describe("with ", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit.with).toBe(tuple.with);
+      });
+    });
+    describe("[Symbol.iterator]", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit[Symbol.iterator]).toBe(tuple[Symbol.iterator]);
+      });
+    });
+    describe("[Symbol.unscopables]", () => {
+      it("should be the same as of the encapsulated tuple", () => {
+        expect(unit[Symbol.unscopables]).toBe(tuple[Symbol.unscopables]);
+      });
+    });
+
+    describe('functional utility compatibility', () => {
+      describe('isTuple', () => {
+        it('should return false for the encapsulated tuple', () => {
+          expect(t.isTuple(unit)).toBe(false);
+        });
+      });
+
+      describe('value', () => {
+        it('should return the value of the encapsulated tuple', () => {
+          expect(t.value(unit)).toBe(5.5);
+        });
+      });
+
+      describe('unit', () => {
+        it('should return the unit of the encapsulated tuple', () => {
+          expect(t.unit(unit)).toBe("in");
+        });
+      });
+
+      describe('toFixed', () => {
+        it('should return the value of the encapsulated tuple with the expected number of fraction digits', () => {
+          expect(t.toFixed(unit, 2)).toEqual([5.5, "in"]);
+        });
+      });
+
+      describe('toExponential', () => {
+        it('should return the value of the encapsulated tuple in exponential notation with the expected number of fraction digits', () => {
+          expect(t.toExponential(unit, 2)).toEqual([5.5, "in"]);
+        });
+      });
+
+      describe('toPrecision', () => {
+        it('should return the value of the encapsulated tuple with the expected precision', () => {
+          expect(t.toPrecision(unit, 2)).toEqual([5.5, "in"]);
+        });
+      });
+    });
   });
 });
