1d6db3f152ad9b864a3820e609e48c958de8fa3e
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-08-26 11:39:46 +0200

    🧹 Cleared things off for a blank slate.

--- src/dimension/dimension.ts
+++
@@ -1,3 +0,0 @@
-import {type Measurement} from "./measurement-type";
-
-export type Dimension<Unit extends string> = {[unit in Unit]: Measurement<Unit, unit>};
--- src/dimension/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./dimension";
-export * from "./measurement";
-export * from "./measurement-dimension";
-export {type Measurement} from "./measurement-type";
-export * from "./measurement-unit";
