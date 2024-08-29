2b76d378c268fd9a068a0e0f056ed9153fa39ab7
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-08-29 03:30:37 +0200

    🏗️ Forgot to update this test.

--- test/unit/unit.test.ts
+++ test/unit/unit.test.ts
@@ -10,16 +10,13 @@ describe("unit", () => {
   });
 
   it("should return the unit of an object measurement.", () => {
-    expect(unit({value: 1, unit: "m"})).toBe("m");
+    expect(unit({value: 1, unit: "m"} as any)).toBe("m");
   });
 
   it("should return the unit of a dimension measurement.", () => {
     const v = 1;
     const u = "m";
-    expect(unit({value: v, unit: u, [u]: v})).toBe("m");
+    expect(unit({value: v, unit: u, [u]: v} as any)).toBe("m");
   });
 
-  it("should throw an error for a non-measurement.", () => {
-    expect(() => unit(1 as any)).toThrow("Failed to resolve the unit of the measurement.");
-  });
 });
