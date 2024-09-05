f93baaabc0eee9fb1d811a1c8552737751ad2d18
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-09-05 16:24:09 +0200

    🏗️ Specialized measurement type exports added.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ export * from "./to-fixed";
 export * from "./to-precision";
 export * from "./unit";
 export * from "./value";
+
+export * as dimension from "./dimension";
+export * as object from "./object";
+export * as string from "./string";
+export * as tuple from "./tuple";
